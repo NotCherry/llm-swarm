d48d97259164b599c3e7d5b7f694172317be867b
Dawid Wiśniewski <dw949@wp.pl>
2025-04-30 20:51:06 +0200

    init

--- server.py
+++ server.py
@@ -5,6 +5,12 @@ import time
 import asyncio
 import logging
 from rich.logging import RichHandler
+import tinygrad
+import transformers
+import time
+from dataclasses import dataclass 
+import requests
+import io
 
 
 
@@ -30,8 +36,6 @@ def get_outbound_ip():
     return ip
 
 
-
-# Set your own listening port and the peer's IP/port
 LISTEN_IP = '0.0.0.0'     # Listen on all interfaces
 LISTEN_PORT = 5005        # Port to listen on
 
@@ -39,16 +43,28 @@ local_address = ".".join(get_outbound_ip().split(".")[:3])
 
 SEARCH_IP_RANGE = [f"{local_address}.{i}" for i in range(1,254) ] # Change to the peer's IP
 PEER_PORT = 5005          # Change to the peer's port
-CONNECT = "connect".encode()
 
 ACTICE_HOSTS = []
+MASTER_NODE = True
+
+START_TIME = time.perf_counter()
+WAIT_TIME = 10
+
+
 
 def listen(sock):
-    global ACTICE_HOSTS
+    global ACTICE_HOSTS, START_TIME, MASTER_NODE
+    
     while True:
         data, addr = sock.recvfrom(1024)
-        if data.decode() == "connect":
+        text = data.decode()
+        if text  == "connect":
             ACTICE_HOSTS.append(addr[0])
+  
+        if ((START_TIME + WAIT_TIME) > time.perf_counter()) and text == "MASTERNODE":
+            log.info(f"Master node address: {addr[0]}")
+            MASTER_NODE = False
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1048576)  # 1 MB buffer
@@ -57,7 +73,10 @@ sock.bind((LISTEN_IP, LISTEN_PORT))
 
 # Start listener thread
 def update_network():
-    global ACTICE_HOSTS
+    global ACTICE_HOSTS, MASTER_NODE
+
+    CONNECT = "connect".encode()
+    MASTER_BRODCAST = "MASTERNODE".encode()
 
     def detect():
         log.info(f"Discovering hosts on: {local_address}.0")
@@ -67,6 +86,11 @@ def update_network():
         log.info(ACTICE_HOSTS)    
     
     while True:
+        if START_TIME + WAIT_TIME < time.perf_counter():
+            log.debug("Brodcasting Master Node")
+            for host in SEARCH_IP_RANGE:
+                sock.sendto(MASTER_BRODCAST, (host, PEER_PORT))
+        log.info(f"Node Type {'Master' if MASTER_NODE else 'Worker'}")
         ACTICE_HOSTS = []
         detect()
         time.sleep(5)
@@ -77,8 +101,35 @@ def swarm_discover(sock):
     threading.Thread(target=update_network).start()
     
 
+def model_downloader_to_ram():
+    url = "https://huggingface.co/Qwen/Qwen2-1.5B/resolve/main/model.safetensors"
+    # url = "https://huggingface.co/Qwen/Qwen2-1.5B/raw/main/merges.txt"
+    metadata_end = 0
+    size_read = False
+
+    buffer = io.BytesIO()
+    with open('downloaded_file.txt', 'wb') as f:
+        # Make a GET request to the server with streaming enabled
+        response = requests.get(url, stream=True)
+        
+        # Check if the request was successful
+        if response.status_code == 200:
+            for chunk in response.iter_content(chunk_size=1024):
+                print(int.from_bytes(chunk[:8]))
+                buffer.write(chunk)
+                if not size_read:
+                    metadata_end = int.from_bytes(buffer.read(8))
+                    print(metadata_end)
+                break
+            print("Download completed successfully.")
+        else:
+            print("Failed to retrieve the file. HTTP Status Code:", response.status_code)
+
+
 async def main():
-    Process(target=swarm_discover, args=(sock,)).start()
+    # Process(target=swarm_discover, args=(sock,)).start()
+    # Process(target=)
+    model_downloader_to_ram()
 
 
 if __name__ == "__main__":
