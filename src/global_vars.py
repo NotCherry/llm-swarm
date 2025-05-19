1ffff3ca17025de271f46bc868e2749b017a8c75
Dawid Wiśniewski <dw949@wp.pl>
2025-05-19 20:00:24 +0200

    chore: removed comments

--- src/global_vars.py
+++ src/global_vars.py
@@ -8,7 +8,6 @@ from src.local_logger import log
 def get_outbound_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        # Doesn't need to actually send data; just triggers routing logic
         s.connect(('1.1.1.1', 80))
         ip = s.getsockname()[0]
     except Exception:
@@ -20,16 +19,16 @@ def get_outbound_ip():
     return ip
 
 
-LISTEN_IP = '0.0.0.0'     # Listen on all interfaces
-LISTEN_PORT = 5005        # Port to listen on
+LISTEN_IP = '0.0.0.0'
+LISTEN_PORT = 5005
 WS_PORT = 8543
 
 LOCAL_ADDRESS = get_outbound_ip()
 PROGRAM_MINIMAL_SPACE = (500 * 1024 * 1024)
 MASTER_NODE_BUFFER = (600 * 1024 * 1024)
 
-SEARCH_IP_RANGE = [f"{".".join(LOCAL_ADDRESS.split(".")[:3])}.{i}" for i in range(1,254) ] # Change to the peer's IP
-PEER_PORT = 5005          # Change to the peer's port
+SEARCH_IP_RANGE = [f"{".".join(LOCAL_ADDRESS.split(".")[:3])}.{i}" for i in range(1,254) ]
+PEER_PORT = 5005
 
 ACTIVE_HOSTS = []
 MASTER_NODE = True
